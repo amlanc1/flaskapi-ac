61271f86c6f20e3b4cf8de2c753d74f22595613b
Amlan <amlana21@gmail.com>
2020-06-06 15:27:27 -0700

    a

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,18 +3,19 @@ from flask_pymongo import PyMongo
 import os
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.getenv('MONGO_URL')
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = os.getenv('MONGO_URL')
+# mongo = PyMongo(app)
 
 @app.route("/")
 def home_page():
-    all_users = mongo.db.users.find()
-    users=[]
-    for rw in all_users:
-        rw.pop('_id')
-        users.append(rw)
-    print(users)
-    resp=make_response(jsonify(users),200)
+    # all_users = mongo.db.users.find()
+    # users=[]
+    # for rw in all_users:
+    #     rw.pop('_id')
+    #     users.append(rw)
+    # print(users)
+    # resp=make_response(jsonify(users),200)
+    resp=make_response(jsonify(status='done'),200)
     return resp
 
 if __name__=='__main__':
